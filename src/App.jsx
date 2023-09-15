4f01e7017e2da906919b493436ff5e2a6ea2860d
unknown <rohanrifat43@gmail.com>
2023-09-15 17:30:14 +0600

    create live link CNAME

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-
-import { useState } from 'react'
-import './App.css'
-import Bookmarks from './Components/Bookmarks/Bookmarks'
-import Courses from './Components/Courses/Courses'
-import Header from './Components/Header/Header'
+import { useState } from "react";
+import "./App.css";
+import Bookmarks from "./Components/Bookmarks/Bookmarks";
+import Courses from "./Components/Courses/Courses";
+import Header from "./Components/Header/Header";
 
 function App() {
-const [bookmarks,setBookmarks]=useState([])
- const handleAddToBookmark=course=>{
-const newCourse=[...bookmarks,course];
-setBookmarks(newCourse)
- }
+  const [bookmarks, setBookmarks] = useState([]);
+  const handleAddToBookmark = (course) => {
+    const newCourse = [...bookmarks, course];
+    setBookmarks(newCourse);
+  };
   return (
     <>
       <Header></Header>
-      
-      <div className='md:flex max-w-7xl mx-auto'>
-      <Courses handleAddToBookmark={handleAddToBookmark}></Courses>
-      <Bookmarks bookmarks={bookmarks}></Bookmarks>
+
+      <div className="md:flex max-w-7xl mx-auto">
+        <Courses handleAddToBookmark={handleAddToBookmark}></Courses>
+        <Bookmarks bookmarks={bookmarks}></Bookmarks>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 
 const Bookmark = ({bookmark}) => {
     const {course_name}=bookmark;
-    let counter=0;
-    counter++
     return (
         
         <div className='shadow-2xl rounded-xl'>
