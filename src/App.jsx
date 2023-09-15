2ef32e9c51c7efc058a2cee8c8e28099a9a73540
unknown <rohanrifat43@gmail.com>
2023-09-15 14:59:41 +0600

    Heading part

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='w-7xl mx-auto text-center mt-4'>
+            <h1 className='text-4xl font-bold'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
