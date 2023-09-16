1b014c06510e4b055b26061b97181678ab6f713f
unknown <rohanrifat43@gmail.com>
2023-09-16 12:07:47 +0600

    Project Done

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -2,12 +2,12 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-const Bookmark = ({ bookmark }) => {
+const Bookmark = ({ bookmark,index }) => {
   const { course_name } = bookmark;
   return (
     <div className="shadow-2xl rounded-xl ml-10">
-      <ol style={{listStyle:'decimal'}}>
-        <li>{course_name}</li>
+      <ol className="text-start font-medium">
+        <li>{index}.{course_name}</li>
       </ol>
     </div>
   );
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -7,15 +7,15 @@ import Bookmark from "../Bookmark/Bookmark";
 
 const Bookmarks = ({ bookmarks, creditHour, creditRemaining }) => {
   return (
-    <div className="md:w-[330px] ml-4 bg-slate-100 mt-8 p-4 text-center">
+    <div className="md:w-[330px] ml-4 bg-slate-100 mt-8 p-4 ">
       <div className="border-b-2 mt-2 text-blue-500">
         <h2 className="text-xl font-semibold ">
           Credit Hour Remaining {creditRemaining}hr
         </h2>
       </div>
-      <h1 className="text-2xl text-center p-4 font-semibold">Course Name</h1>
-      {bookmarks.map((bookmark) => (
-        <Bookmark bookmark={bookmark}></Bookmark>
+      <h1 className="text-2xl p-4 font-semibold">Course Name</h1>
+      {bookmarks.map((bookmark,index) => (
+        <Bookmark index={index+1} bookmark={bookmark}></Bookmark>
       ))}
       <div className="border-t-2 mt-2">
         <h2 className="text-xl font-semibold mt-2">
